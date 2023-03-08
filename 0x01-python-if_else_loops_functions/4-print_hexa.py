0fa30e422470dea7a12cc8b6dbf5fd56bcdf9d8a
Kiaella <emmanuellaodoemenam9@gmail.com>
2023-03-07 19:14:14 -0800

    task 4

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#Author - Odoemenam El
+
+"""Print numbers of 0 to 98 in decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
