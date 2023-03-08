8bd0f35b01e205ca3c87a87f04067424e13f572e
Kiaella <emmanuellaodoemenam9@gmail.com>
2023-03-07 17:52:04 -0800

    task 0

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#Author - Odoemenam El
+def add(a, b):
+    """Return the addition of a and b"""
+    return (a +ab)
+
---
+++ 0x01-python-if_else_loops_functions/10-main.py
@@ -0,0 +1,7 @@
+#!/usr/env/python3
+add = _import_ ('10-add').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
