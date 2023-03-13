95898b31f1599c044ff2fc5017124ff6a83a1500
Kiaella <emmanuellaodoemenam9@gmail.com>
2023-03-13 01:20:16 -0700

    task 0

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,7 +17,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next
-}listint_t
+} listint_t
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -26,4 +26,4 @@ void free_listint(listint_t *head);
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
-#endif /LISTS_H/
+#endif /* LISTS_H */
